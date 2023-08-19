a3915f989ad0fe1777567c073405f1b62965ef8f
IsCan <104559952+iskandar13abdurakhmonov@users.noreply.github.com>
2023-08-19 13:09:53 +0500

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a0ba253603msh1b05321d1800af5p192e6bjsn20c6ec55075e",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "a8086fc55d8c501c81f95b1936faaf91";
